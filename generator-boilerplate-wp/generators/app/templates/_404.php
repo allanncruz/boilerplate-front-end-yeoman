323fe4ac98e3252af66a35d32194557952df6bf5
allanncruz <contato@allancruz.com.br>
2019-11-21 14:21:12 -0300

    Update structure 404 pages

--- generator-boilerplate-wp/generators/app/templates/_404.php
+++ generator-boilerplate-wp/generators/app/templates/_404.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
 
-    <section class="wrap">
-        <div class="container content-area mt-5">
-            <div class="content-box bg-white p-md-5 p-3  shadow-sm">
-                <div class="row">
-                    <div class="col-12 col-md-12">
-                        <h1 class="display-4 text-center">Página não encontrada!</h1>
-                        <p>O caminho para o conteúdo que você deseja aessar está errado ou a página podeter sido exluída</p>
-                    </div>
-                </div>
-            </div>
+    <section class="site-content">
+        <div class="container text-center">
+            <h1>Página não encontrada!</h1>
+            <p>O caminho para o conteúdo que você deseja aessar está errado ou a página podeter sido exluída</p>
         </div>
     </section>
 
