aa451d6231d650941d19cabf28319fef5dfa9f75
allanncruz <contato@allancruz.com.br>
2020-03-15 14:28:41 -0300

    update navbar and create navmodal mobile version

--- generator-boilerplate-wp/generators/app/templates/_header.php
+++ generator-boilerplate-wp/generators/app/templates/_header.php
@@ -1,37 +1,70 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
+    <!-- Requered meta tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta <?php bloginfo('charset'); ?> >
-    <title>><?php bloginfo('title') ?></title>
+    <meta <?php bloginfo('charset'); ?>>
+
+    <title><?php bloginfo('title') ?></title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body class="bg-light">
 
 <header class="header position-fixed w-100 shadow">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container">
             <a class="navbar-brand text-white" href="<?php bloginfo("url") ?>">
-                <?php the_custom_logo() ?>
+                <?php 
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+
+
+                    if(has_custom_logo()) {
+                        echo '<img src="'. esc_url($logo[0]). '" class="img-fluid">';
+                    }else {
+                        echo '<p class="m-0 text-white">'. get_bloginfo('name'). '</p>';
+                    } 
+                ?>
             </a>
-            <button class="navbar-toggler border-0 collapsed rounded-0 p-1"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1"
-                    aria-controls="bs-example-navbar-collapse-1"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span class="my-1 w-100 close"><i class="fas fa-times"></i></span>
-                <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
-            </button>
+
+            <div class="d-blok d-lg-none">
+                <button type="button" class="modal-button border-0" data-toggle="modal" data-target="#navModal">
+                <i class="fas fa-bars"></i>
+                </button>
+
+                <div class="modal fade" id="navModal" tabindex="-1" role="dialog" aria-labelledby="navModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="navModalLabel">Menu</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?php
+                                wp_nav_menu( array(
+                                    'theme_location'    => 'principal',
+                                    'depth'             => 2,
+                                    'container'         => 'div',
+                                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                    'walker'            => new WP_Bootstrap_Navwalker(),
+                                ) );
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <?php
             wp_nav_menu( array(
                 'theme_location'    => 'principal',
                 'depth'             => 2,
                 'container'         => 'div',
                 'container_class'   => 'collapse navbar-collapse',
-                'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
+                'menu_class'        => 'nav navbar-nav ml-auto',
                 'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                 'walker'            => new WP_Bootstrap_Navwalker(),
             ) );
