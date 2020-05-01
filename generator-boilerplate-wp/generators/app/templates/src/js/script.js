d8c5e51a4e543c3c3558f2fe0e81a57ee74218cd
allanncruz <contato@allancruz.com.br>
2020-05-01 18:23:33 -0300

    upgrade showcase section

--- generator-boilerplate-wp/generators/app/templates/src/js/script.js
+++ generator-boilerplate-wp/generators/app/templates/src/js/script.js
@@ -19,8 +19,9 @@
         }
     });
 
-    $('.owl-carousel.carousel--anima').owlCarousel({
+    $('.owl-carousel.banner').owlCarousel({
         nav: true,
+        dots: true,
         loop: true,
         navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
         items:1,
@@ -35,3 +36,18 @@
 
     $(".blocks-gallery-item figure a").attr("rel", "galeria");
     $(".blocks-gallery-item figure  a").fancybox();
+
+    jQuery("input.telefone")
+        .mask("(99) 9999-99999")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 10) {  
+                element.mask("(99) 99999-9999");  
+            } else {  
+                element.mask("(99) 9999-99999");  
+            }  
+        });
