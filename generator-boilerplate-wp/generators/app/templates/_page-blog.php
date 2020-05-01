b967fcbee50f6d6ce7c1e6cb7e167da4bcd3fd21
allanncruz <contato@allancruz.com.br>
2020-05-01 18:26:35 -0300

    add partial blig section and update cards styles

--- generator-boilerplate-wp/generators/app/templates/_page-blog.php
+++ generator-boilerplate-wp/generators/app/templates/_page-blog.php
@@ -1,6 +1,6 @@
-<?php get_header(); ?>
-
-<?php the_post() ?>
+<?php
+    get_header();
+    the_post() ?>
 
     <section class="wrap">
         <div class="container content-area mt-5">
@@ -11,7 +11,12 @@
             
             <div class="row justify-content-center">
                 <div class="col-md-10">
-                    <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" class="single-thumbnail my-4">
+                    <img
+                        onerror="this.style.display='none'"
+                        src="<?php the_post_thumbnail_url(); ?>"
+                        class="single-thumbnail my-4"
+                        alt="<?php the_title(); ?>"
+                    >
                     <article class="px-md-5 px-2">
                         <?php the_content(); ?>
                     </article>
@@ -22,24 +27,7 @@
                         $blog = new WP_Query(array( 'post_type' => 'blog' ));
                         if ($blog->have_posts()):
                             while ($blog->have_posts()): $blog->the_post(); ?>
-                            <div class="col-md-4">
-                                <a href="<?php the_permalink(); ?>">
-                                    <div class="cards-item mx-1">
-                                        <div class="">
-                                            <img class="cards-item__image" onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" />
-                                        </div>
-                                        <div class="cards-item__legend px-4 py-3">
-                                            <div class="cards-item__data text-white">
-                                                <?php the_time( get_option( 'date_format' ) ); ?>
-                                            </div>
-                                            <span class="card-category mt-3">
-                                                <?php echo get_the_term_list( get_the_ID(), 'blog_taxonomy', '#', ', ' ); ?>
-                                            </span>
-                                            <h5 class="cards-item__title"><?php the_title(); ?></h5>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
+                                <?php get_template_part( 'partials/section', 'news' ); ?>
                         <?php endwhile; endif; ?>
                     </div>
                 </div>
--- generator-boilerplate-wp/generators/app/templates/_single.php
+++ generator-boilerplate-wp/generators/app/templates/_single.php
@@ -1,6 +1,6 @@
-<?php get_header(); ?>
-
-<?php the_post() ?>
+<?php
+    get_header();
+    the_post() ?>
 
     <section class="wrap">
         <div class="container content-area mt-5">
@@ -11,9 +11,26 @@
             
             <div class="row justify-content-center">
                 <div class="col-md-10">
-                    <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" class="single-thumbnail my-4">
+                    <img
+                        onerror="this.style.display='none'"
+                        src="<?php the_post_thumbnail_url(); ?>"
+                        class="single-thumbnail my-4"
+                        alt="<?php the_title(); ?>"
+                    >
                     <article class="px-md-5 px-2">
                         <?php the_content(); ?>
+
+                        <hr>
+                        <small>Post relacionado</small>
+                        <?php
+                        $relationship = get_field('showcase-relationship');
+                        if( $relationship ): ?>
+                            <?php foreach( $relationship as $post): ?>
+                                <?php get_template_part( 'partials/section', 'news' ); ?>
+                            <?php endforeach;
+                            wp_reset_postdata();
+                        endif; ?>
+
                         <div class="row w-100">
                             <div class="col-md-6">
                                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php the_permalink(); ?>" data-a2a-title="<?php the_title(); ?>">
